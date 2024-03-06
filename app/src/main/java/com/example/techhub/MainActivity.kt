918cb488246c7b895686ed227324f4dbb5b9c75a
MRESENDESILVA <mateus.rsilva@sptech.school>
2024-03-05 21:37:48 -0300

    add - adicionando componentes para tela de cadastro

--- app/src/main/java/com/example/techhub/MainActivity.kt
+++ app/src/main/java/com/example/techhub/MainActivity.kt
@@ -3,13 +3,43 @@ package com.example.techhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.techhub.ui.theme.TechHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +50,127 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    Tela()
                 }
+            }
+        }
+    }
+
+
+
+    @Composable
+    fun Tela(){
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(painter = painterResource(
+                id = R.mipmap.fundo),
+                contentDescription = "Background azul",
+                modifier = Modifier.matchParentSize(),
+                alignment = Alignment.TopStart
+            )
+            
+            Column (modifier = Modifier.padding(horizontal = 20.dp, vertical = 16.dp)
+            ){
+                Row {
+                    IconeFreelancer(height = 148.0, width = 148.0)
+                }
+                DefaultButton(height = 48.0, width = 311.0)
+            }
+        }
+
+
+    }
+
+
+    @Composable
+    fun BottomAppBarExample() {
+        Scaffold(
+            bottomBar = {
+                BottomAppBar(
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(Icons.Filled.Check, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                Icons.Filled.Edit,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                Icons.Filled.AccountBox,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                Icons.Filled.AddCircle,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = { /* do something */ },
+                            containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+                        ) {
+                            Icon(Icons.Filled.Add, "Localized description")
+                        }
+                    }
+                )
+            },
+        ) { innerPadding ->
+            Text(
+                modifier = Modifier.padding(innerPadding),
+                text = "Example of a scaffold with a bottom app bar."
+            )
+        }
+    }
+
+    @Composable
+    fun DefaultButton(height: Double, width: Double){
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(height.dp)
+                .width(width.dp),
+            shape = MaterialTheme.shapes.small,
+        ) {
+            Text(text = "Avançar")
+        }
+    }
+    
+    @Composable
+    fun IconeFreelancer(height: Double, width: Double) {
+        Box(modifier = Modifier
+            .height(height.dp)
+            .width(width.dp)
+            .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(painter = painterResource(id = R.mipmap.freelancer),
+                    contentDescription = "Imagem freelancer",
+                    modifier = Modifier.fillMaxSize()
+                )
+
+                Text(text = "Teste",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(4.dp),
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TechHubTheme {
-        Greeting("Android")
-    }
-}
